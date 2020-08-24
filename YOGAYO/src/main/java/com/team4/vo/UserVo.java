0a57f8cb753ca1425e799116a853edffc358b844
leeminjaee <32377931+leeminjaee@users.noreply.github.com>
2020-08-24 20:18:01 +0900

    mongo!!!

--- YOGAYO/src/main/java/com/team4/vo/UserVo.java
+++ YOGAYO/src/main/java/com/team4/vo/UserVo.java
@@ -4,13 +4,22 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.conversions.Bson;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
-
+import com.mongodb.client.model.Filters;
 
 public class UserVo {
 
@@ -22,29 +31,64 @@ public class UserVo {
 	char gender;
 	String img;
 	Date regDate;
-	
+
 	public UserVo() {
 	}
-	
+
 	public static void main(String[] args) {
-		MongoClient  mongo = new MongoClient("localhost",27017);
+		MongoClientURI uri = new MongoClientURI(
+				"mongodb+srv://user:12345@cluster0.9bcza.mongodb.net/test?retryWrites=true&w=majority");
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("test");
+		MongoClient mongo = new MongoClient("localhost", 27017);
 		MongoDatabase test = mongo.getDatabase("test");
-        //3. 컬렉션 가져오기(Bson 형태)
-        MongoCollection<Document> collection = test.getCollection("user");
-        String b="안녕하세요!!";
-       List<String> a= new ArrayList<String>();
-       a.add("안");
-       a.add("녕");
-       a.add("하");
-       a.add("세");
-       a.add("요");
-        Document doc= new Document("_id",1771717).append("keyword", a  );
-        collection.insertOne(doc);
-        System.out.println("성공");
-		
-	
+		// 3. 컬렉션 가져오기(Bson 형태)
+		MongoCollection<Document> collection = database.getCollection("user");
+		insert(collection);
+		String b = "안녕하세요!!";
+		// insert(collection);
+		DB db = mongoClient.getDB("test");
+		DBCollection cltn = db.getCollection("user");
+		find(cltn);
+
 	}
-	
+
+	public static void find(DBCollection collection) {
+		System.out.println("1. Find all matched documents");
+		DBCursor cursor = collection.find();
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
+
+		System.out.println();
+
+		System.out.println("2. get 'keyword' filed only");
+
+		BasicDBObject allquery = new BasicDBObject();
+		BasicDBObject fields = new BasicDBObject();
+		allquery.put("_id", 1771717);
+		fields.put("keyword", 1);
+		DBCursor cursor2 = collection.find(allquery, fields);
+		while (cursor2.hasNext()) {
+			DBObject obj = cursor2.next();
+			List<String> list = (List<String>) obj.get("keyword");
+			System.out.println(list);
+		}
+
+	}
+
+	public static void insert(MongoCollection<Document> collection) {
+
+		List<String> a = new ArrayList<String>();
+		a.add("안");
+		a.add("녕");
+		a.add("하");
+		a.add("세");
+		a.add("요");
+		Document doc = new Document("_id", 1771717).append("keyword", a);
+		collection.insertOne(doc);
+		System.out.println("성공");
+	}
 
 	public UserVo(int uNum, String id, String pwd, String name, Date bDate, char gender, String img, Date regDate) {
 		super();
@@ -57,8 +101,6 @@ public class UserVo {
 		this.img = img;
 		this.regDate = regDate;
 	}
-
-
 
 	@Override
 	public String toString() {
@@ -129,7 +171,5 @@ public class UserVo {
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
-	
-	
 
 }
