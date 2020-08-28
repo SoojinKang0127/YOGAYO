e27c022c5692886be37e1a887194aaab6fffff83
SoojinKang0127 <iamhajaml@naver.com>
2020-08-28 12:56:42 +0900

    commit

--- YOGAYO/src/main/java/com/team4/test/PlayerController.java
+++ YOGAYO/src/main/java/com/team4/test/PlayerController.java
@@ -47,22 +47,20 @@ public class PlayerController {
 	
 	
 	
-
-	
 	@RequestMapping(value = "/feedupload", method = RequestMethod.POST)
 	public String feedupload(MultipartHttpServletRequest multi, HttpServletRequest req) {
 		
 		
-//		HttpSession session = req.getSession();
-//		UserVo user = (UserVo)session.getValue("user");
-//		int uNum = user.getuNum();
+		HttpSession session = req.getSession();
+		UserVo user = (UserVo)session.getValue("user");
+		int uNum = user.getuNum();
 		
 		
-//		int  course = Integer.parseInt(multi.getParameter("course"));
-//		int slevel = Integer.parseInt(multi.getParameter("slevel"));
-//		int  dlevel = Integer.parseInt(multi.getParameter("dlevel"));
-//		String context = multi.getParameter("context");
-//		int  weight = Integer.parseInt(multi.getParameter("weight"));
+		int  course = Integer.parseInt(multi.getParameter("course"));
+		int slevel = Integer.parseInt(multi.getParameter("slevel"));
+		int  dlevel = Integer.parseInt(multi.getParameter("dlevel"));
+		String context = multi.getParameter("context");
+		int  weight = Integer.parseInt(multi.getParameter("weight"));
 		
 		MultipartFile mf = multi.getFile("file");
 		String originalFileName = mf.getOriginalFilename();
@@ -72,13 +70,14 @@ public class PlayerController {
 		System.out.println(fileType);
 		System.out.println(multi.getRealPath("/"));
 		
-		//Users/soo/Documents/YOGAYO/YOGAYO/YOGAYO/src/main/webapp/resources/image/feedimages
-		//String testPath="C://Users/kosmo//document//github//yogayo/yogayo/.metadat////"
+		
 		String realPath = "/Users/soo/Documents/YOGAYO/YOGAYO/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/YOGAYO/";
 		String curUserPath=realPath.substring(0, realPath.indexOf(".metadata"));
 		System.out.println(curUserPath);
 		String path = curUserPath +"YOGAYO/src/main/webapp/resources/image/feedimages/";
+		String projectPath = "resources/image/feedimages/";
 		System.out.println(path);
+		
 		int no=0;
 		try {
 			 no= service.getLastFnum()+1;
@@ -87,47 +86,39 @@ public class PlayerController {
 		}
 		String safeFile = path + "feed"+Integer.toString(no)+fileType;
 		
-//		vo.setuNum(uNum);
-//		vo.setCrsNum(course);
-//		vo.setsLevel(slevel);
-//		vo.setdLevel(dlevel);
-//		vo.setContext(context);
-//		vo.setWeight(weight);
-//		vo.setuImg1(safeFile);
-//		
-//		
+		String prjPathandName = projectPath + "feed"+Integer.toString(no)+fileType;
 		
-		String message = "";
+		
 		
 		try {
 			mf.transferTo(new File(safeFile));
-			message = "파일 업로드 성공";
+			System.out.println("파일 들어감");
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
-			message = "오류";
 		} catch (IOException e) {
 			e.printStackTrace();
-			message = "오류";
+			System.out.println("오류! 파일 안 들어감");
 		}
 		
+				
+		vo.setuNum(uNum);
+		vo.setCrsNum(course);
+		vo.setsLevel(slevel);
+		vo.setdLevel(dlevel);
+		vo.setContext(context);
+		vo.setWeight(weight);
+		vo.setuImg1(prjPathandName);
 		
 		
+		try {
+			service.upload(vo);
+			System.out.println("피드 들어감");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("오류 피드 안 들어감");
+		}
 		
-		
-		
-//		try {
-//			service.upload(vo);
-//			System.out.println("피드 들어감");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("오류! 피드 안 들어감");
-//		}
-		
-
-		
-
-		
-		return "test";
+		return "main";
 	}
 	
 	
