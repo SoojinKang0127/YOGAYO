cbfa23dbee0d562fc9aada120ba7e5396bcb342e
vlvethund <50507749+vlvethund@users.noreply.github.com>
2020-09-11 12:52:24 +0900

    123

--- YOGAYO/src/main/java/com/team4/util/CosineSimilarity.java
+++ YOGAYO/src/main/java/com/team4/util/CosineSimilarity.java
@@ -36,69 +36,64 @@ public class CosineSimilarity implements R {
 		DBCollection userDB = mongoDBf.getCollection("USER");
 		Rcontroller controller = new Rcontroller();
 		controller.updateKeyword();
-		ArrayList<ArrayList<Integer>> listAll = new ArrayList<ArrayList<Integer>>();
+		ArrayList<CSArray> listAll = new ArrayList<CSArray>();
 
 		BasicDBObject myQuery = new BasicDBObject("_id", vo.getuNum());
 		DBCursor myCursor = userDB.find(myQuery, field);
 		DBObject myobj = myCursor.next();
 		List<String> myList = (List<String>) myobj.get("keyword");
-		ArrayList<Integer> myintlist = new ArrayList<Integer>(Collections.nCopies(keywordList.size() + 2, 0));
-		myintlist.set(0, vo.getuNum());
-		myintlist.set(1, myList.size());
+		CSArray mycsarray = new CSArray(keywordList.size());
+		mycsarray.getList().set(0, vo.getuNum());
+		mycsarray.getList().set(1, myList.size());
 		for (int j = 0; j < myList.size(); j++) {
 			String key = myList.get(j);
 			int index = keywordList.indexOf(key);
-			myintlist.set(index + 2, 1);
+			mycsarray.getList().set(index + 2, 1);
 		}
 		for (int i = 0; i < listExceptMe.size(); i++) {
 			BasicDBObject userQuery = new BasicDBObject("_id", listExceptMe.get(i));
 			DBCursor userCursor = userDB.find(userQuery, field);
 			DBObject obj = userCursor.next();
 			List<String> userList = (List<String>) obj.get("keyword");
-			ArrayList<Integer> intlist = new ArrayList<Integer>(Collections.nCopies(keywordList.size() + 2, 0));
-			intlist.set(0, listExceptMe.get(i));
-			intlist.set(1, userList.size());
+			CSArray csarray = new CSArray(keywordList.size());
+			csarray.getList().set(0, listExceptMe.get(i));
+			csarray.getList().set(1, userList.size());
 			for (int j = 0; j < userList.size(); j++) {
 				String key = userList.get(j);
 				int index = keywordList.indexOf(key);
-				intlist.set(index + 2, 1);
+				csarray.getList().set(index + 2, 1);
 			}
-			listAll.add(intlist);
-		}
-
-		for (int i = 0; i < listAll.size(); i++) {
-			ArrayList<Integer> csList = listAll.get(i);
 			int total = 0;
-			for (int j = 2; j < csList.size(); j++) {
-				total += csList.get(j) * myintlist.get(j);
+			for (int j = 2; j < csarray.getList().size(); j++) {
+				total += csarray.getList().get(j) * mycsarray.getList().get(j);
 			}
-			System.out.println("겹치는 개수:" + total);
-			System.out.print(csList.get(0) + "의 코사인 유사도는");
-			System.out.print(total / (Math.sqrt(csList.get(1)) * Math.sqrt(myintlist.get(1))));
-			System.out.println();
+			csarray.setKeywordcs(total / (Math.sqrt(csarray.getList().get(1)) * Math.sqrt(mycsarray.getList().get(1))));
+			listAll.add(csarray);
 		}
-
+// like cs 시작
+		
+		
 		List<Integer> courseListExceptme = null;
 		List<Integer> myCourse = null;
 		LikeVo mylvo = new LikeVo();
 		List<Integer> courseList = null;
 		ArrayList<Integer> myCourseList = null;
 		mylvo.setuNum(vo.getuNum());
-		ArrayList<ArrayList<Integer>> courseListAll = new ArrayList<ArrayList<Integer>>();
+		ArrayList<CSArray> courseListAll = new ArrayList<CSArray>();
 		try {
 			myCourse = lservice.countCourse(mylvo);
 			courseList = cservice.courseAll();
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
-			myCourseList = new ArrayList<Integer>(Collections.nCopies(courseList.size() + 2, 0));
-			myCourseList.set(0, vo.getuNum());
-			myCourseList.set(1, myCourse.size());
-			for (int j = 0; j < myCourse.size(); j++) {
-				int key = myCourse.get(j);
-				int index = courseList.indexOf(key);
-				myCourseList.set(index + 2, 1);
-			}
+		myCourseList = new ArrayList<Integer>(Collections.nCopies(courseList.size() + 2, 0));
+		myCourseList.set(0, vo.getuNum());
+		myCourseList.set(1, myCourse.size());
+		for (int j = 0; j < myCourse.size(); j++) {
+			int key = myCourse.get(j);
+			int index = courseList.indexOf(key);
+			myCourseList.set(index + 2, 1);
+		}
 
 		System.out.println("코스 리스트 얼: " + courseList.toString());
 		System.out.println("마이코스 : " + myCourse.toString());
@@ -106,7 +101,7 @@ public class CosineSimilarity implements R {
 		for (int i = 0; i < listExceptMe.size(); i++) {
 			LikeVo lvo = new LikeVo();
 			lvo.setuNum(listExceptMe.get(i));
-			ArrayList<Integer> intlist = new ArrayList<Integer>(Collections.nCopies(courseList.size() + 2, 0));
+			CSArray intlist = new CSArray(courseList.size());
 			try {
 				courseListExceptme = lservice.countCourse(lvo);
 
@@ -117,25 +112,39 @@ public class CosineSimilarity implements R {
 			for (int j = 0; j < courseListExceptme.size(); j++) {
 				int key = courseListExceptme.get(j);
 				int index = courseList.indexOf(key);
-				intlist.set(index + 2, 1);
+				intlist.getList().set(index + 2, 1);
 			}
-			intlist.set(0, listExceptMe.get(i));
-			intlist.set(1, courseListExceptme.size());
+			intlist.getList().set(0, listExceptMe.get(i));
+			intlist.getList().set(1, courseListExceptme.size());
+			
+			int total = 0;
+			for (int j = 2; j < intlist.getList().size(); j++) {
+				total += intlist.getList().get(j) * myCourseList.get(j);
+			}
+			intlist.setLikecs(total / (Math.sqrt(intlist.getList().get(1)) * Math.sqrt(myCourseList.get(1))));
 			courseListAll.add(intlist);
 		}
 
-		for (int i = 0; i < courseListAll.size(); i++) {
-			ArrayList<Integer> csList = courseListAll.get(i);
-			int total = 0;
-			for (int j = 2; j < csList.size(); j++) {
-				total += csList.get(j) * myCourseList.get(j);
-			}
-			System.out.println("겹치는 개수:" + total);
-			System.out.print(csList.get(0) + "의 코사인 유사도는");
-			System.out.print(total / (Math.sqrt(csList.get(1)) * Math.sqrt(myintlist.get(1))));
-			System.out.println();
-		}
 
+		for(int i=0;i<listAll.size();i++) {
+			for(int j=0;j<courseListAll.size();j++) {
+				if(courseListAll.get(j).getList().get(0)==listAll.get(i).getList().get(0))
+					courseListAll.get(j).setKeywordcs(listAll.get(i).getKeywordcs());
+			}
+		}
+		Collections.sort(courseListAll);
+		System.out.println(courseListAll.toString());
+		
+		ArrayList<Integer> myRecommend=myCourseList;
+		ArrayList<Integer> recommendCourse= new ArrayList<Integer>();
+		
+		for(int i=2;i<myRecommend.size();i++) {
+			myRecommend.set(i,myRecommend.get(i)-courseListAll.get(0).getList().get(i));
+			if(myRecommend.get(i)==-1)
+				recommendCourse.add(i-1);
+		}
+		System.out.println(myRecommend.toString());
+		System.out.println(recommendCourse.toString());
 	}
 
 }
